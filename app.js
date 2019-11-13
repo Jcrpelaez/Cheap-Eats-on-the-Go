60007e02d223f156e80173c89dca4ec0d91c41cc
jmike2689 <jmike2689@yahoo.com>
2019-11-12 21:03:49 -0600

    Updated getPlaces function

--- app.js
+++ app.js
@@ -1,34 +1,59 @@
 //ready document for JQuery
-$(document).ready(function() {
-  var address = "";
+$(document).ready(function () {
+  var street = "";
   var city = "";
   var state = "";
-  var zip = "";
+  //var zip = "";
   // Button click functions Add button and Clear buttons
-  $("#findPlaces").on("click", function(event) {
+  $("#findPlaces").on("click", function (event) {
     event.preventDefault();
-    address = $("#address").val();
+    street = $("#address").val();
     city = $("#city").val();
     state = $("#state").val();
     zip = $("#zip").val();
-    
+
     hideForm();
-    getPlaces();
+    getPlaces(street, city, state);
   });
 });
 
-function hideForm(){
-    $('#myDiv').hide();
+function hideForm() {
+  $('#myDiv').hide();
 }
 
-function showmap(){
-    //calls array of points to drop markers
+function showmap() {
+  //calls array of points to drop markers
 }
 
-function getPlaces() {
-    //finds 10 places and stores an array of points
+function getPlaces(street, city, state) {
+  //finds 10 places and stores an array of points
+  var address = encodeURIComponent(street + "," + " " + city + "," + " " + state);
+  console.log(address);
+  var bedrooms = 4;
+  var bathrooms = 2;
+  var propertyType = "Single%20Family";
+  var squareFootage = 1600;
+  var results = 10;
+
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://realtymole-rental-estimate-v1.p.rapidapi.com/rentalPrice?address=" + address + "&compCount=10",
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "realtymole-rental-estimate-v1.p.rapidapi.com",
+      "x-rapidapi-key": "c403db74fcmsh82c18191678d823p1aeb6ajsneba4140264a8"
+    }
+  }
+
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    //$("#info").text(response.listings[0].address);
+
+  });
 
 
-    //show map
-    showmap();
+
+  //show map
+  showmap();
 }
