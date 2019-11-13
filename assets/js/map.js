b9d0ebb1dd2b62428586351d3e4389b0b9dcee65
Juan Carlos <jcrpelaez@yahoo.com>
2019-11-12 21:01:57 -0600

    added multiple markers

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,11 +1,52 @@
+// function initMap() {
+//   // The location of the UTSA Campus
+//   var sanAntonio = { lat: 29.424122, lng: -98.493629 };
+//   var markerOne = { lat: 30, lng: -80 };
+//   var markerTwo = { lat: 28, lng: -100 };
+//   // The map, centered at San Antonio
+//   var map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 4,
+//     center:
+//   });
+//   // // The marker, positioned at UTSA
+//   // var marker = new google.maps.Marker({ position: sanAntonio, map: map });
+//   var marker = new google.maps.Marker({ position: markerOne, map: map });
+//   var marker = new google.maps.Marker({ position: markerTwo, map: map });
+// }
+var marker;
+
 function initMap() {
-  // The location of Uluru
-  var sanAntonio = { lat: 29.424122, lng: -98.493629 };
-  // The map, centered at Uluru
   var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: sanAntonio
+    zoom: 6,
+    center: { lat: 31.968599, lng: -99.90181 }
   });
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({ position: sanAntonio, map: map });
+
+  marker = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: { lat: 31.327, lng: -99.067 }
+  });
+  marker.addListener("click", toggleBounce);
+
+  markerTwo = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: { lat: 31.324, lng: -99.069 }
+  });
+  marker.addListener("click", toggleBounce);
+
+  markerThree = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: { lat: 31.328, lng: -99.066 }
+  });
+  marker.addListener("click", toggleBounce);
+}
+
+function toggleBounce() {
+  if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
 }
