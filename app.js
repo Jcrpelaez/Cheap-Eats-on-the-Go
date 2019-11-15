9d54f7d090f31c0865dcdeb631347f9a3c9742c1
jmike2689 <jmike2689@yahoo.com>
2019-11-14 21:41:46 -0600

    Adding updated app.js and search.html files

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ function getPlaces(street, city, state) {
   var address = encodeURIComponent(street + "," + " " + city + "," + " " + state);
   console.log(address);
 
+  var listings = [];
+
+  //Ajax call being set up
   var settings = {
     "async": true,
     "crossDomain": true,
@@ -41,9 +44,63 @@ function getPlaces(street, city, state) {
     }
   }
 
+  //Ajax call taking place and displaying results
   $.ajax(settings).done(function (response) {
     console.log(response);
-    //$("#info").text(response.listings[0].address);
+
+    $("#results").append("<table class='table'><thead><tr><th scope='col'>Address</th><th scope='col'>Bedroom</th><th scope='col'>Bathroom</th><th scope='col'>Price</th></tr></thead><tbody>");
+
+    for (x = 0; x < response.listings.length; x++) {
+      //console.log("test");
+
+      $("#results").append("<tr><");
+
+      results = [
+        response.listings[x].latitude,
+        response.listings[x].longitude,
+        response.listings[x].formattedAddress,
+        response.listings[x].bedrooms,
+        response.listings[x].bathrooms,
+        response.listings[x].price
+      ];
+
+      for (y = 2; y < results.length; y++) {
+        $("#results").append("<td>" + results[y] + "</td>");
+      };
+
+      $("#results").append("</tr>");
+
+
+      console.log(results[x]);
+
+    };
+
+    $("#results").append("</tbody></table>");
+
+    // var rows = [];
+
+    // function makeRows() {
+
+    //   for (i = 2; i < results.length; i++) {
+    //     var $row = $("<tr></tr>");
+    //     $row.append($("<td></td>")).text(results[i]);
+    //     $row.push(rows)
+    //   };
+
+    //   $("#results").append(rows)
+
+    //   };
+
+    // var $row = $("<tr></tr>");
+
+    // $row.append($("<td></td>")).text(listing[2]);
+    // $row.append($("<td></td>")).text(listing[5]);
+
+    // $tableBody.append($row);
+
+    // $("#results").after($tableBody);
+
+
 
   });
 
