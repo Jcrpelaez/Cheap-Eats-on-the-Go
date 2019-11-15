ff83e02dee5ee6daaf9e5b842da845778c135c9d
Juan Carlos <jcrpelaez@yahoo.com>
2019-11-14 20:29:31 -0600

    created for loop for marker array

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,16 +1,23 @@
-function init(){
-  var mapOptions={
-    center: new google.maps.LatLng(40,-73),
-    mapTypeId: google.maps.mapTypeId.ROADMAP,
-    zoom:13
-  };
-  venueMap = new google.maps.map(document.getElementById('map'), mapOptions);
-}
+var locations = [
+  ["The Alamo", 29.425594, -98.274856],
+  ["The Missions", 29.36186, -98.47923],
+  ["UTSA DownTown", 29.422933, -98.502937]
+];
 
-function loadScript(){
-  var script = document
-}
-var points = [];
-for (var i = 0; i < latitude.length; i++) {
-  points.push(new google.maps.LatLng(latitude[i], longitude[i]));
+// var marker;
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: { lat: 29.424122, lng: -98.493629 }
+  });
+
+  for (i = 0; i < locations.length; i++) {
+    var marker = new google.maps.Marker({
+      map: map,
+      draggable: false,
+      animation: google.maps.Animation.DROP,
+      position: { lat: locations[i][1], lng: locations[i][2] }
+    });
+  }
 }
